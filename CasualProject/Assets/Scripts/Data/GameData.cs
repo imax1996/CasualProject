742eacda3e9651f21d56d75afe8275e39502f27d
Max <78084694+imax1996@users.noreply.github.com>
2021-09-29 12:17:43 +0300

    Clear project

--- CasualProject/Assets/Scripts/Data/GameData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameData : MonoBehaviour
-{
-    public GameObject Menu;
-    public LevelData LevelData;
-    public Fade Fade;
-}
